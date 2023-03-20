29046ec8d2b6b5c5a53285af324f32c1b86d6a98
Arslan <a.kalaitsidis@msensis.com>
2023-03-20 16:47:52 +0200

    Add prototype pattern

---
+++ KotlinExamples/DesignPatterns/src/main/kotlin/creational/prototype.kt
@@ -0,0 +1,9 @@
+package creational
+
+data class PC(
+    val motherboard: String = "Terasus XZ27",
+    val cpu: String = "Until Atom K500",
+    val ram: String = "8GB Microcend BBR5",
+    val graphicCard: String = "nKCF 8100TZ"
+)
+
