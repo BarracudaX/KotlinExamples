08cbbd0db45d0927128aa25decc6a36ec15d4fbd
Arslan <a.kalaitsidis@msensis.com>
2023-03-20 08:36:48 +0200

    Add example of static factory method desgin pattern

---
+++ KotlinExamples/DesignPatterns/src/main/kotlin/staticFactoryMethod.kt
@@ -0,0 +1,18 @@
+class MyClass private constructor(){
+
+
+    companion object{
+        private lateinit var cached:MyClass
+
+        fun getNewOrCached() : MyClass{
+
+            if(!this::cached.isInitialized){
+                cached = MyClass()
+            }
+
+            return cached
+        }
+
+    }
+
+}
