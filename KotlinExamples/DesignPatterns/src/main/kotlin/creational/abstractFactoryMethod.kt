2831918c1eab84c62397403d81c315b4b3301ab9
Arslan <a.kalaitsidis@msensis.com>
2023-03-21 15:10:25 +0200

    Remove previous examples and add new.
    1)Removed all previous examples - they were badly written.
    2)Added new example of abstract factory pattern.

---
+++ KotlinExamples/DesignPatterns/src/main/kotlin/creational/abstractFactoryMethod.kt
@@ -0,0 +1,49 @@
+package creational
+
+/**
+ * Abstract Factory Pattern allows superclass to focus on business logic using some abstraction, while allowing subclass to specify the actual class of the abstraction that is used by the superclass.
+ */
+
+interface Transport{
+
+    fun transport(cargo: String)
+
+}
+
+class Ship : Transport{
+    override fun transport(cargo: String) {
+        println("Transporting cargo $cargo over sea.")
+    }
+
+}
+
+class Truck : Transport{
+    override fun transport(cargo: String) {
+        println("Transporting cargo $cargo with a truck.")
+    }
+
+}
+
+
+abstract class LogisticService{
+
+    protected abstract fun newTransport() : Transport
+
+
+    fun transport(cargo: String){
+        val transport = newTransport()
+
+        transport.transport(cargo)
+    }
+
+}
+
+class SeaLogisticService : LogisticService(){
+    override fun newTransport(): Transport = Ship()
+
+}
+
+class TruckLogisticService : LogisticService(){
+    override fun newTransport(): Transport = Truck()
+
+}
--- KotlinExamples/DesignPatterns/src/main/kotlin/creational/builder.kt
+++
@@ -1,5 +0,0 @@
-package creational
-
-import java.io.File
-
-data class Mail( val to: String,val cc: List<String> = emptyList(),val bcc: List<String> = emptyList(),val title: String? = null, val message: String = "",val attachments: List<File> = emptyList())
