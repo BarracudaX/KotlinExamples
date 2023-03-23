d6cb81e52b0e9d9245f11b99588c6911ccdbce3b
Arslan <a.kalaitsidis@msensis.com>
2023-03-23 19:12:09 +0200

    Add strategy design pattern example

---
+++ KotlinExamples/DesignPatterns/src/main/kotlin/behavioral/stategy.kt
@@ -0,0 +1,47 @@
+package behavioral
+
+/**
+ * Strategy design pattern allows defining a family of interchangeable algorithms that is used by class and,optionally,change this algorithm at runtime.
+ */
+
+interface TaxesService{
+
+    fun taxed(money: Double) : Double
+
+}
+
+class GreeceTaxesService : TaxesService{
+    override fun taxed(money: Double): Double = money - (money*0.15)
+
+}
+
+class GermanyTaxesService : TaxesService{
+    override fun taxed(money: Double): Double {
+        return if(money < 1000){
+            money - (money*0.20)
+        }else if(money < 5000){
+            money - (money*0.25)
+        }else{
+            money - (money*0.30)
+        }
+    }
+
+}
+
+class PaymentService(private val taxesService: TaxesService){
+
+    private val regularPayment = 1000.0
+
+    fun getPayment(hours: Int) : Double{
+        return if(hours <= 50){
+            taxesService.taxed(regularPayment)
+        }else if(hours <= 100){
+            val additionalHours = hours - 50
+            taxesService.taxed(regularPayment + 40*additionalHours)
+        }else{
+            val additionalHours = hours - 50
+            taxesService.taxed(regularPayment + 40*additionalHours+200)
+        }
+    }
+
+}
